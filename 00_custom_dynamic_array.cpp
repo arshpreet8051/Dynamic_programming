4994f45df1c43e6cd1e24493ba472bdc37570553
Arshpreet_Singh <arshpreet8051@gmail.com>
2023-12-24 23:17:35 +0530

    Custom dynamic vector

--- 00_custom_dynamic_array.cpp
+++ 00_custom_dynamic_array.cpp
@@ -1,31 +1,57 @@
 #include<iostream>
-#include<vector>
-//#include<bits/stdc++.h>
+
 using namespace std;
-#define or |
-#define and &
- 
-// creating my own vector
-class vector{
 
-//data members
-int size;
-int capacity;
+class Vector {
+public:
+    int size;
+    int capacity;
+    int* arr;
 
-//constructor
-vector(){
-    this.size =1;
-}
+    Vector() {
+        this->size = 1;
+        this->capacity = 0;
+        arr = new int[size];
+    }
 
+    Vector(int size) {
+        this->size = size;
+        this->capacity = 0;
+        arr = new int[size];
+    }
 
+    ~Vector() {
+        delete[] arr;
+    }
+
+    bool push(int x) {
+        if (this->capacity == this->size) {
+            int* temp = new int[size * 2];
+            for (int i = 0; i < size; i++) {
+                temp[i] = arr[i];
+            }
+            this->size *= 2;
+            delete[] arr; // Release old memory
+            arr = temp;
+        }
+        arr[capacity++] = x;
+        return true;
+    }
+
+    int pop() {
+        if (capacity <= 0) {
+            return -1;
+        }
+        return arr[--capacity];
+    }
 };
 
-
-int main()
-{
-
-
-
+int main() {
+    Vector v = Vector();
+    cout << v.push(1) << " " << v.size << " " << v.capacity << endl;
+    cout << v.push(2) << " " << v.size << " " << v.capacity << endl;
+    cout << v.push(3) << " " << v.size << " " << v.capacity<<endl;
+    cout << v.push(3) << " " << v.size << " " << v.capacity<<endl;
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+#define or |
+// #define and &
